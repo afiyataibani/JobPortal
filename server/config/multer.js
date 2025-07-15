349fbbb362b12825a24fa37edd2a4fd1e7af64b3
afiyataibani <afiyataibani@gmail.com>
2025-07-15 14:49:40 +0530

    Completed

--- server/config/multer.js
+++ server/config/multer.js
@@ -1,15 +1,6 @@
-// import multer from "multer";
-
-// const storage = multer.diskStorage({});
-
-// const upload = multer({ storage });
-
-// export default upload;
-
-// config/multer.js
 import multer from "multer";
 
-const storage = multer.memoryStorage(); // ✅ use memory storage instead of disk
+const storage = multer.diskStorage({});
 
 const upload = multer({ storage });
 
--- server/server.js
+++ server/server.js
@@ -7,6 +7,9 @@ import * as Sentry from "@sentry/node";
 import { clerkWebhooks } from "./controllers/webhooks.js";
 import companyRoutes from "./routes/companyRoutes.js";
 import connectCloudinary from "./config/cloudinary.js";
+import jobRoutes from "./routes/jobRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import { clerkMiddleware } from "@clerk/express";
 
 // Initialize Express
 const app = express();
@@ -18,6 +21,7 @@ await connectCloudinary();
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(clerkMiddleware());
 
 // Routes
 app.get("/", (req, res) => res.send("API Working"));
@@ -26,6 +30,8 @@ app.get("/debug-sentry", function mainHandler(req, res) {
 });
 app.post("/webhooks", clerkWebhooks);
 app.use("/api/company", companyRoutes);
+app.use("/api/jobs", jobRoutes);
+app.use("/api/users", userRoutes);
 
 // Port
 const PORT = process.env.PORT || 5050;
